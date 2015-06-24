043ad327347c58c2ff50f38c29c8b20e29eb6e57
Nanoman <nanoman689@gmail.com>
2015-06-23 20:03:20 -0700

    Slight changes to @media
    Made a change to @media settings for the main image.
    Added an InLine Image Solution.

--- build/js/app.js
+++ build/js/app.js
@@ -1,1 +1,1 @@
-$("body").velocity("fadeIn",{duration:1500}),$("h1").velocity("slideup",{duration:2100}).velocity("slideDown",{duration:2500});
+$("body").velocity("fadeIn",{duration:1500}),$("h1").velocity("slideDown",{duration:2500});
